cddde8dcbcfe50e25f88107c3894b05f2ebb7f17
muqeetkhan <muqeetkhan050@gmail.com>
2026-01-17 04:25:04 +1000

    removed error for production

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import LeftBar from "./Leftbar";
 
 import { useNavigate } from "react-router-dom";
 const Home=()=>{
-    const navigate=useNavigate();
+
     return(
         <div>
            
--- src/pages/Leftbar.js
+++ src/pages/Leftbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Streak from '../components/Streak';
 import Timer from '../components/Timer';
+import Notes from '../components/Notes';
 export default function LeftBar() {
   return (
     <div >  
         <Streak />      
         <Timer />   
+        <Notes />
      
     </div>
   );
