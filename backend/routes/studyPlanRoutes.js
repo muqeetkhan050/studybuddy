8e4fccb9d0dcbde5bf1d34e0d82f85026450a037
muqeetkhan <muqeetkhan050@gmail.com>
2026-01-21 22:30:25 +1000

    actions

--- backend/routes/studyPlanRoutes.js
+++ backend/routes/studyPlanRoutes.js
@@ -1,18 +1,3 @@
-// import express from "express";
-// import authMiddleware from "../middleware/authMiddleware.js";
-// import {
-//   createPlan,
-//   getPlans,
-//   deletePlan
-// } from "../controllers/studyPlanController.js";
-
-// const router = express.Router();
-
-// router.post("/", authMiddleware, createPlan);
-// router.get("/", authMiddleware, getPlans);
-// router.delete("/:id", authMiddleware, deletePlan);
-
-// export default router;
 
 
 import express from "express";
@@ -21,7 +6,6 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Protect all study plan routes with JWT middleware
 router.get("/", protect, getPlans);
 router.post("/", protect, createPlan);
 router.delete("/:id", protect, deletePlan);
