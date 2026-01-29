21ca100944eb99d0cfea2307778eb3d8631a12ef
muqeetkhan <muqeetkhan050@gmail.com>
2026-01-30 08:15:53 +1000

    a

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/authContext';
+
 import API from '../services/api';
 
 const RegisterPage = () => {
